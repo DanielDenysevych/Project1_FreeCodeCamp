c5f72360b595b1791371387812029bd99ae774b0
DanielDenysevych <151660075+DanielDenysevych@users.noreply.github.com>
2025-01-23 12:02:10 -0600

    Update URL_shortner.js

--- URL_shortner.js
+++ URL_shortner.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
+// Url Shortener
 app.post('/api/shorturl', function(req, res) {
   console.log(req.body)
   const url = req.body.url
